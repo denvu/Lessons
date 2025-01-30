78155699301a73439dd4063d7d2245b44342d34f
Valeriya <leradenisenko.9@gmail.com>
2025-01-30 19:45:40 +0600

    Урок 4.9

--- cart.js
+++ cart.js
@@ -2,10 +2,9 @@
 
 const cart = {
   items: [],
-  totalPrice: 0,
   count: 0,
-  getTotalPrice() {
-    return this.totalPrice;
+  get totalPrice() {
+    return this.calculateItemPrice();
   },
   add(name, price, amount = 1) {
     const goods = {
@@ -14,19 +13,17 @@ const cart = {
       amount: amount,
     };
     this.items.push(goods);
-
-    this.count += amount;
-
-    this.totalPrice += price * amount;
   },
   increaseCount(num) {
     this.count += num;
   },
   calculateItemPrice() {
-    this.totalPrice = this.items.reduce((sum, item) => {
+    this.count = this.items.reduce((quantity, item) => {
+      return quantity + item.amount;
+    }, 0);
+    return this.items.reduce((sum, item) => {
       return sum + item.price * item.amount;
     }, 0);
-    return this.totalPrice;
   },
   clear() {
     this.items = [];
@@ -37,11 +34,11 @@ const cart = {
     const itemsJSON = JSON.stringify(this.items);
     console.log(itemsJSON);
 
-    console.log(`Общая стоимость корзины: ${this.totalPrice}`);
+    console.log(`Общая стоимость корзины: ${cart.totalPrice}`);
   },
 };
 
-cart.add('Товар 1', 100);
-cart.add('Товар 2', 200, 2);
-cart.add('Товар 3', 300, 5);
+cart.add("Товар 1", 100);
+cart.add("Товар 2", 200, 2);
+cart.add("Товар 3", 300, 5);
 cart.print();
