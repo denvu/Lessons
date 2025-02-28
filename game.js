68138d1dafd62932b4fd0c7fda2a4c5692829108
Valeriya <leradenisenko.9@gmail.com>
2025-02-28 17:54:27 +0600

    Исправлено

--- game.js
+++ game.js
@@ -1,48 +1,67 @@
 'use strict';
 
-const getRandomIntInclusive = () => {
-  const min = 1;
-  const max = 10;
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const game = () => {
-  const result = {
-    player: 5,
-    computer: 5,
+(() => {
+  const getRandomIntInclusive = () => {
+    const min = 1;
+    const max = 10;
+    return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  console.log(result);
 
-  const playGame = () => {
-    const number = Number(prompt(`Загадайте число от 1 до ${result.player}`));
+  const game = () => {
+    const result = {
+      player: 5,
+      computer: 5,
+    };
+    console.log(result);
 
-    if (result.computer > 0 && result.player > 0) {
-      const randomNumber = getRandomIntInclusive();
+    const playGame = () => {
+      let number;
+      if (result.player < result.computer) {
+        number = Number(prompt(`Загадайте число от 1 до ${result.player}`));
+      } else {
+        number = Number(prompt(`Загадайте число от 1 до ${result.computer}`));
+      }
 
-      if (
-        (number % 2 === 0 && randomNumber % 2 === 0) ||
+      if (isNaN(number) ||
+      number < 1 ||
+      number > Math.min(result.player, result.computer)) {
+        // eslint-disable-next-line max-len
+        alert(`Введите число от 1 до ${Math.min(result.player, result.computer)}`);
+        return playGame();
+      }
+
+      if (result.computer > 0 && result.player > 0) {
+        let randomNumber = getRandomIntInclusive();
+
+        if (result.player === 1) {
+          randomNumber = 1;
+        }
+
+        if (
+          (number % 2 === 0 && randomNumber % 2 === 0) ||
           (number % 2 !== 0 && randomNumber % 2 !== 0)
-      ) {
-        result.player -= number;
-        result.computer += number;
-      } else {
-        result.player += number;
-        result.computer -= number;
-      }
+        ) {
+          result.player -= number;
+          result.computer += number;
+        } else {
+          result.player += number;
+          result.computer -= number;
+        }
 
-      console.log(result);
+        console.log(result);
 
-      if (result.computer <= 0) {
-        alert(`Игра окончена! Вы выиграли!`);
-      } else if (result.player <= 0) {
-        alert(`Игра окончена! Вы проиграли!`);
-      } else {
-        playGame();
+        if (result.computer === 0) {
+          alert(`Игра окончена! Вы выиграли!`);
+        } else if (result.player === 0) {
+          alert(`Игра окончена! Вы проиграли!`);
+        } else {
+          playGame();
+        }
       }
-    }
+    };
+
+    playGame();
   };
 
-  playGame();
-};
-
-game();
+  window.balls = game;
+})();
