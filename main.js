730cdb705c61f36981d75f937b98f90cba492306
Valeriya <leradenisenko.9@gmail.com>
2025-03-08 18:34:22 +0600

    push list

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-'use strict';
+"use strict";
 
-const ads = document.querySelector('.ads');
-ads.remove();
+const ul = document.querySelector(".ul");
 
-const items = document.querySelector('.items');
-const item = document.querySelectorAll('.item');
-const titles = document.querySelectorAll('.item__title');
-const propsLists = document.querySelectorAll('.props__list');
-const propsItemsTwo = document.querySelectorAll('.props__item_two');
-const propsItemsFour = document.querySelectorAll('.props__item_four');
+const creatList = () => {
+  const query = prompt("Введите строку");
 
-const cloneList3 = propsLists[3].cloneNode(true);
-const cloneList5 = propsLists[4].cloneNode(true);
+  const creatLi = () => {
+    const li = document.createElement("li");
+    li.textContent = query;
+    ul.append(li);
 
-item[3].after(item[0]);
-propsItemsFour[2].after(propsItemsFour[5]);
-propsLists[3].replaceWith(cloneList5);
-propsLists[4].replaceWith(cloneList3);
-propsLists[2].append(propsItemsTwo[8]);
-propsLists[2].append(propsItemsTwo[9]);
-titles[2].innerText = "This и прототипы объектов";
-titles[4].replaceWith(titles[1]);
-titles[3].replaceWith(titles[4]);
-propsLists[2].insertAdjacentHTML('beforebegin', `<h2 class="item__title">Область видимости и замыкание</h2>`)
+    creatList();
+  };
+
+  const deleteLi = () => {
+    const lastLi = ul.lastElementChild;
+      ul.removeChild(lastLi);
+  };
+
+  const clearList = () => {
+    ul.innerHTML = "";
+      return ul;
+  }
+
+  if (query === "del") {
+    deleteLi();
+  } else if(query === "clear") {
+    clearList();
+  } else if (query === "exit" || query === null) {
+    return false;
+  } else {
+    creatLi();
+  }
+};
+
+creatList();
